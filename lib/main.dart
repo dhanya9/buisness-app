cbc5feb327cc99886fbc078c03f3b2d4af2b3505
dhanya9 <garbmode09@gmail.com>
2021-06-28 21:53:43 +0530

    Day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
  import 'package:flutter/material.dart';
+ import 'package:flutter_aap/pages/login_page.dart';
 
+ import 'pages/home_page.dart';
  void main(){
    runApp(MyApp());  
  }
@@ -8,13 +10,12 @@
 
    Widget build(BuildContext context){
      return MaterialApp(
-       home: Material(
-         child: Center(
-          child: Container(
-           child: Text('welcome to buissness'),
-          )
-        )
-      )
+       routes: {
+        // "/" = FOR DEFAULT PAGE 
+         "/": (context) => LoginPage(),
+         "/home": (context) => HomePage(),
+         "/login": (context) => LoginPage()
+       },
      );  
    }
  }
