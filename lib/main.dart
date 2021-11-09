3951fc314cf8e2e175cd3e8f60435c54cda23b43
dhanya9 <garbmode09@gmail.com>
2021-11-09 22:29:56 +0530

    day 6 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
- import 'package:flutter/material.dart';
- import 'package:flutter_aap/pages/login_page.dart';
-
- import 'pages/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_aap/pages/add_emp_page.dart';
+import 'package:flutter_aap/pages/client_page.dart';
+import 'package:flutter_aap/pages/my_emp_page.dart';
+import 'package:flutter_aap/pages/login_page.dart';
+import 'package:flutter_aap/utils/routes.dart';
+import 'pages/home_page.dart';
  void main(){
    runApp(MyApp());  
  }
@@ -10,11 +13,17 @@
 
    Widget build(BuildContext context){
      return MaterialApp(
+       debugShowCheckedModeBanner: false,  //remove debug tag
+       initialRoute: MyRoutes.homeRoute,
        routes: {
         // "/" = FOR DEFAULT PAGE 
          "/": (context) => LoginPage(),
-         "/home": (context) => HomePage(),
-         "/login": (context) => LoginPage()
+         MyRoutes.homeRoute: (context) => HomePage(),
+         MyRoutes.loginRoute: (context) => LoginPage(),
+         MyRoutes.myempRoute: (context) => MyempPage(),
+         
+         MyRoutes.addempRoute: (context) => Addemppage(),
+         MyRoutes.clientdataRoute: (context) => Clientpage()
        },
      );  
    }
